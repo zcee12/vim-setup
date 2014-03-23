f05ba2e48b81730b6c3752f24d64be5bbf14a2bc
zceee12 <richardclose@gmail.com>
2014-03-23 21:45:45 +0000

    removing install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-if [ -d "~/.vim" ]; then
-    mkdir -p ${HOME}/.vim/autoload ${HOME}/.vim/bundle; \
-        curl -Sso ${HOME}/.vim/autoload/pathogen.vim \
-            https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-else
-    mkdir ${HOME}/.vim
-fi
-
-cp vimrc ${HOME}/.vimrc
