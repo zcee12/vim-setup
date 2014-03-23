a1db428c1e1ab94c4eb5c2cb498ab9e4f68fcf95
zceee12 <richardclose@gmail.com>
2014-03-23 21:44:42 +0000

    switched to using vundle

--- update.sh
+++ update.sh
@@ -1,7 +1,19 @@
-echo "Pulling changes from git"
+# Set up the .vim directory
+echo "Checking for .vim directory"
+if [ ! -d ${HOME}/.vim ]; then
+    mkdir ${HOME}/.vim
+fi
+
+echo "Pulling down latest pathogen"
+rm -rf ${HOME}/.vim/bundle/vundle
+git clone git@github.com:gmarik/Vundle.vim.git ${HOME}/.vim/vundle
+
+echo "Pulling latest custom changes from git"
 git pull
 
 echo "Copying latest vimrc to ~/.vimrc"
 cp -v vimrc ${HOME}/.vimrc
 echo "Done"
 
+echo 'Installing plugins'
+vim +PluginInstall +qall
