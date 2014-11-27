1bbce461177b7e0c497c79ddf146394c66dda5e0
zceee12 <richardclose@gmail.com>
2014-11-27 15:51:28 +0000

    adding reith update script

---
+++ reith-update.sh
@@ -0,0 +1,19 @@
+# Set up the .vim directory
+echo "Checking for .vim directory"
+if [ ! -d ${HOME}/.vim ]; then
+    mkdir ${HOME}/.vim
+fi
+
+echo "Pulling down latest pathogen"
+rm -rf ${HOME}/.vim/bundle/vundle
+git clone r/github:gmarik/Vundle.vim.git ${HOME}/.vim/bundle/vundle
+
+echo "Pulling latest custom changes from git"
+git pull
+
+echo "Copying latest vimrc to ~/.vimrc"
+cp -v vimrc ${HOME}/.vimrc
+echo "Done"
+
+echo 'Installing plugins'
+vim +PluginInstall +qall
